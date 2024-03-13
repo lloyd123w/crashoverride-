f4907de7b6d2bd1b1c5631b346a7107830040857
lloyd123w <153415017+lloyd123w@users.noreply.github.com>
2024-03-13 14:51:33 +0800

    Update loginpage.js

--- components/loginpage.js
+++ components/loginpage.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const LoginPage = ({ navigation }) => {
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleLogin = () => {
     // Here you can implement your login logic, such as validating credentials
     console.log('Email:', email);
+    console.log('Password:', password); // Check password in console
     navigation.navigate('homepage'); // Navigate to the homepage
     // Implement your login logic here...
   };
@@ -29,15 +33,26 @@ const LoginPage = ({ navigation }) => {
         value={email}
         onChangeText={setEmail}
       />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry
-      />
+      <View style={styles.passwordContainer}>
+        <TextInput
+          style={styles.passwordInput}
+          placeholder="Password"
+          secureTextEntry={!showPassword}
+          value={password}
+          onChangeText={setPassword}
+        />
+        <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+          <MaterialIcons
+            name={showPassword ? 'visibility-off' : 'visibility'}
+            size={24}
+            color="#000"
+          />
+        </TouchableOpacity>
+      </View>
       <TouchableOpacity onPress={handleLogin}>
         <Text style={styles.button}>Login</Text>
       </TouchableOpacity>
-      <Text style={styles.forgotPassword} onPress={handleForgotPassword}>Forgot Password?</Text> {/* Link/button for forgot password */}
+      <Text style={styles.forgotPassword} onPress={handleForgotPassword}>Forgot Password?</Text>
       <Text style={styles.signUpText}>Don't have an account? </Text>
       <TouchableOpacity onPress={handleSignUp}>
         <Text style={styles.signUpLink}>Sign Up</Text>
@@ -64,6 +79,20 @@ const styles = StyleSheet.create({
     borderColor: '#ccc',
     borderRadius: 5,
     marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  passwordContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    marginBottom: 10,
+  },
+  passwordInput: {
+    flex: 1,
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
     paddingHorizontal: 10,
   },
   button: {
